4a245fc09f2751cad8319163a65b70d4deeb7f91
Aaron Levine <allevin@sandia.gov>
2019-09-20 12:30:43 -0600

    Remove test text

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'The Structural Simulation Toolkit' /* title for your website */,
-  tagline: 'Using the supercomputers of today to build the supercomputers of tomorrow - XXX',
+  tagline: 'Using the supercomputers of today to build the supercomputers of tomorrow',
   //url: 'https://your-docusaurus-test-site.com' /* your website url */,
   //baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
