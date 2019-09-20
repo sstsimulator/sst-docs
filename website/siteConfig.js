8cf72078d510adc6495a3de921cb1575763ae7fb
Aaron Levine <allevin@sandia.gov>
2019-09-20 10:57:28 -0600

    Made some changes to the siteconfig trying to get it to be hosted by Github Pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -27,10 +27,10 @@ const siteConfig = {
   //baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   url: 'https://sstsimulator.github.io',
-  baseUrl: '/SST-website/',
+  baseUrl: '/sst-website/',
 
   // Used for publishing and more
-  projectName: 'SST-website',
+  projectName: 'sst-website',
   organizationName: 'sstsimulator',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
