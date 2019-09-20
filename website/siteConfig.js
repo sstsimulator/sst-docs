8fe5b1954dc560c86f861d5e568fe049ebf139d4
Aaron Levine <allevin@sandia.gov>
2019-09-20 11:08:02 -0600

    Test to ensure that I am changing the correct website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'The Structural Simulation Toolkit' /* title for your website */,
-  tagline: 'Using the supercomputers of today to build the supercomputers of tomorrow',
+  tagline: 'Using the supercomputers of today to build the supercomputers of tomorrow - XXX',
   //url: 'https://your-docusaurus-test-site.com' /* your website url */,
   //baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
